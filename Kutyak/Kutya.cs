cbb25f26b573fdebb9be6abda739e6638380f92d
DevDominik <kecskes.dominik.hun@gmail.com>
2023-05-25 11:48:47 +0200

    4. óra vége

---
+++ Kutyak/Kutya.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    public class Kutya
+    {
+        int kutyaId, fajtaId, nevId, eletkor;
+        string utolsoEllenorzes;
+        public Kutya(string initAdatok)
+        {
+            string[] adatok = initAdatok.Split(';');
+            this.kutyaId = int.Parse(adatok[0]);
+            this.fajtaId = int.Parse(adatok[1]);
+            this.nevId = int.Parse(adatok[2]);
+            this.eletkor = int.Parse(adatok[3]);
+            this.utolsoEllenorzes = adatok[4];
+        }
+    }
+}
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Kutyak;
