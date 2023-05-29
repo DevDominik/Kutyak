7d1d681a2d6f80af61b3ecc5dbe498ce53ba4492
DevDominik <kecskes.dominik.hun@gmail.com>
2023-05-29 19:04:09 +0200

    kész

--- Kutyak/Kutya.cs
+++ Kutyak/Kutya.cs
@@ -8,22 +8,23 @@ namespace Kutyak
 {
     public class Kutya
     {
-        int kutyaId, fajtaId, nevId, eletkor;
+        int kutyaId, fajtaId, nevId;
+        double eletkor;
         string utolsoEllenorzes;
         public Kutya(string initAdatok)
         {
             string[] adatok = initAdatok.Split(';');
-            this.kutyaId = int.Parse(adatok[0]) - 1;
-            this.fajtaId = int.Parse(adatok[1]) - 1;
-            this.nevId = int.Parse(adatok[2]) - 1;
-            this.eletkor = int.Parse(adatok[3]);
+            this.kutyaId = int.Parse(adatok[0]);
+            this.fajtaId = int.Parse(adatok[1]);
+            this.nevId = int.Parse(adatok[2]);
+            this.eletkor = double.Parse(adatok[3]);
             this.utolsoEllenorzes = adatok[4];
         }
 
         public int KutyaId { get => kutyaId; }
         public int FajtaId { get => fajtaId; }
         public int NevId { get => nevId; }
-        public int Eletkor { get => eletkor; }
+        public double Eletkor { get => eletkor; }
         public string UtolsoEllenorzes { get => utolsoEllenorzes; }
 
     }
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -6,13 +6,13 @@ using Kutyak;
 string adatforras = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName}\\Datas\\";
 
 List<Kutya> kutyak = File.ReadAllLines($"{adatforras}Kutyak.csv").Skip(1).Select(x => new Kutya(x)).ToList();
-List<List<string>> nevek = new List<List<string>>();
+List<string> nevek = new List<string>();
 List<List<string>> fajtak = new List<List<string>>();
 StreamReader sr = new StreamReader($"{adatforras}KutyaNevek.csv");
 sr.ReadLine();
 while (!sr.EndOfStream)
 {
-    nevek.Add(sr.ReadLine().Split(';').ToList());
+    nevek.Add(sr.ReadLine().Split(';')[1]);
 }
 sr.Close();
 sr = new StreamReader($"{adatforras}KutyaFajtak.csv");
@@ -22,3 +22,15 @@ while (!sr.EndOfStream)
     fajtak.Add(sr.ReadLine().Split(';').ToList());
 }
 sr.Close();
+Console.WriteLine($"Kutyák neveinek száma: {nevek.Count()}");
+Console.WriteLine($"Kutyák átlag életkora: {Math.Round(kutyak.Average(x => x.Eletkor), 2)}");
+Kutya keresett = kutyak.OrderByDescending(x => x.Eletkor).First();
+Console.WriteLine($"Legidősebb kutya neve és fajtája: {nevek[keresett.NevId - 1]}, {fajtak[keresett.FajtaId - 1][1]} ({fajtak[keresett.FajtaId - 1][2]})");
+Console.WriteLine($"Kutyák száma az orvosnál 2018.01.10-én: {kutyak.Count(x => x.UtolsoEllenorzes == "2018.01.10")}");
+Console.WriteLine($"Legjobban leterhelt nap: ");
+StreamWriter sw = new StreamWriter($"{adatforras}statisztika.txt");
+foreach (Kutya item in kutyak)
+{
+    sw.WriteLine($"{nevek[item.NevId - 1]};{kutyak.GroupBy(y => y.NevId).Count()}");
+}
+sw.Close();
